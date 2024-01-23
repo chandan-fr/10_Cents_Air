08b4dc1197aa72a21fb56a8cc2b5bf731f83adbe
chandan-fr <georgesteve31019@gmail.com>
2024-01-23 20:01:57 +0530

    my trip initiated

--- src/screens/bottomnav/MyTrips.js
+++ src/screens/bottomnav/MyTrips.js
@@ -1,20 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { black } from '../../config/colors';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { black, white } from '../../config/colors';
+import Header from '../../components/Header';
+import BgGradient from '../../utility/BgGradient';
+
+const { width, height } = Dimensions.get("window");
 
 const MyTrips = () => {
   return (
-    <View style={styles.parent}>
-      <Text style={{color: black}}>MyTrips</Text>
-    </View>
+    <SafeAreaView style={styles.parent}>
+      <BgGradient width={width} height={height} />
+      <Header />
+
+      <View style={styles.body}>
+
+      </View>
+    </SafeAreaView>
   )
 };
 
 export default MyTrips;
 
 const styles = StyleSheet.create({
-    parent:{
-        flex: 1,
-        justifyContent: "center",
-    },
+  parent: {
+    flex: 1,
+  },
+  body: {
+    marginTop: 25,
+    flex: 1,
+    backgroundColor: white,
+    marginHorizontal: 4,
+    borderRadius: 8,
+    marginBottom: 80,
+  },
 });
